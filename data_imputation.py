1ced1498f8668d4bd6d9764a550f7e06f0cb5268
dRevan1 <filippokorny699@gmail.com>
2025-12-23 13:20:47 +0100

    Dokončené všetky kroky, imputation aj vyhodnotenie kritéria, ešte prerobiť trochu objektovo ako classu a dať to do cyklu.

--- data_imputation.py
+++ data_imputation.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 import numpy as np
 
 # načíta sa súbor do dataframe, hodnoty ako float, prvý stĺpec (číslo záznamu 1-300) sa dropol
@@ -9,33 +10,43 @@ def get_data_matrix(filepath):
 
     return data_matrix
 
-def get_mean_vector(matrix):
-    col_sums = matrix.T @ np.ones([matrix.shape[0], 1])
-    col_means = col_sums / matrix.shape[0]
-
-    return col_means
-
-def get_std_vector(matrix, mean_vector):
-    std_matrix = matrix - mean_vector.T
-    std_matrix *= std_matrix
-    col_sums = std_matrix.T @ np.ones([std_matrix.shape[0], 1])
-    col_sums /= std_matrix.shape[0]
-    std_vector = np.sqrt(col_sums)
-
-    return std_vector
-
 # matica z matice dát sa získajú indexy s NaN a na ich miesto sa zároveň dajú priemery daného stĺpca záznamu
+# vráti indexy pre kde boli chýbajúce NaN hodnoty
 def init_data_matrix(matrix):
     feature_means = np.nanmean(matrix, axis=0)
     nan_coords = np.where(np.isnan(matrix))
+    valid_coords = np.where(~np.isnan(matrix))
     matrix[nan_coords] = np.take(feature_means, nan_coords[1])
 
-    col_means = get_mean_vector(matrix)
-    col_stds = get_std_vector(matrix, col_means)
-    matrix -= col_means.T
-    matrix /= col_stds.T
+    return nan_coords, valid_coords
 
-    return nan_coords
+# potom sa zavolá už s STD maticou v hlavnom cykle - UPRAVIŤ !!!!
+def compute_PCA(std_matrix):
+    cov_matrix = 1/(std_matrix.shape[0]-1) * ( std_matrix.T @ std_matrix)
+    eigenvalues, eigenvectors = np.linalg.eigh(cov_matrix) # miesto eig, eigh je pre symetrické matice
+    sorted_desc = np.argsort(eigenvalues)[::-1]  # zoradenie zostupne - aby prvý komponent mal najvyšší variance
+    eigenvectors = eigenvectors[:, sorted_desc]
+    
+    return eigenvectors
+
+def get_imputated_matrix(std_data_matrix, eigenvectors, n_components):
+    loadings_matrix = eigenvectors[:, :n_components]
+    scores_matrix = std_data_matrix @ loadings_matrix
+    reconstructed_matrix = scores_matrix @ loadings_matrix.T
+
+    return reconstructed_matrix
+
+def get_objective(original_matrix, imputated_matrix, nan_coords):
+    objective_matrix = original_matrix - imputated_matrix
+    objective_matrix[nan_coords] = 0
+    objective = np.sum(objective_matrix**2)
+    
+    return objective
 
 data_matrix =  get_data_matrix("v3_missing.csv")
-nan_coords = init_data_matrix(data_matrix)
+nan_coords, valid_coords = init_data_matrix(data_matrix)
+std_matrix = StandardScaler().fit_transform(data_matrix)
+eigenvectors = compute_PCA(std_matrix)
+imputated_matrix = get_imputated_matrix(std_matrix, eigenvectors, 1)
+data_matrix[nan_coords] = imputated_matrix[nan_coords]
+objective = get_objective(data_matrix, imputated_matrix, nan_coords)
